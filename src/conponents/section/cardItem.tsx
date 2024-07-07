12a5c2d9d0e540ff3e39c071d25e14959ec1a7fb
nxgthanhcong <73934789+nxgthanhcong@users.noreply.github.com>
2024-07-07 12:47:19 +0700

    update card item

--- src/conponents/section/cardItem.tsx
+++ src/conponents/section/cardItem.tsx
@@ -21,11 +21,11 @@ const CardItem = (props: ICardItemProps) => {
 
   return (
     <Link to={href} onClick={preventDefaultConditionCheck}>
-      <div className="pb-12 block max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 h-full">
+      <div className="pb-8 md:pb-12 block max-w-sm p-3 md:p-4 bg-white border border-gray-200 rounded-md md:rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 h-full">
         <div className="rounded-md overflow-hidden mb-4">
-          <CdlImage id={imgId} />
+          <CdlImage twClass="h-[160px] w-full object-cover" id={imgId} />
         </div>
-        <h5 className="mb-1 text-xl font-medium tracking-tight text-gray-800 dark:text-white">
+        <h5 className="md:mb-1 text-md md:text-xl font-medium tracking-tight text-gray-800 dark:text-white">
           {title}
         </h5>
         {listParagraph.map((item) => item)}
--- src/conponents/section/sectionTitle.tsx
+++ src/conponents/section/sectionTitle.tsx
@@ -6,9 +6,9 @@ interface ISectionTitleProps {
 const SectionTitle = (props: ISectionTitleProps) => {
   const { title, tagTitle } = props;
   return (
-    <h1 className="flex items-center text-3xl font-bold dark:text-white mb-4">
+    <h1 className="flex items-center text-lg md:text-3xl font-bold dark:text-white mb-4">
       {title}
-      <span className="bg-blue-100 text-blue-800 text-sm font-semibold me-2 px-1.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-2 uppercase">
+      <span className="bg-blue-100 text-blue-800 text-xs md:text-sm font-semibold me-2 px-1.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-2 uppercase">
         {tagTitle}
       </span>
     </h1>
